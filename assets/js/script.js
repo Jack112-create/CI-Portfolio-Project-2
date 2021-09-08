a34226ee26077b4117a04fee1b8ad62d78972e99
Jack Conroy <jackconroy777@gmail.com>
2021-09-08 13:52:30 +0000

    Add game screen variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,43 @@
+// Global variables.
+
+// Game screens.
+const homeScreen = document.getElementById('home-screen');
+const gameScreen = document.getElementById('game-screen');
+const bonusScreen = document.getElementById('bonus-game');
+
+// Navigation buttons.
+const startBtn = document.getElementById('start');
+const bonusBtn = document.getElementById('bonus');
+const homeBtn = document.getElementsByClassName('home');
+
+startBtn.addEventListener('click', displayGameScreen);
+bonusBtn.addEventListener('click', displayBonusScreen);
+
+// Loop over home buttons class and add event listener.
+for (home of homeBtn) {
+    home.addEventListener('click', displayHomeScreen);
+};
+
+// Game screen display functions.
+function displayHomeScreen() {
+    bonusScreen.style.display = 'none';
+    gameScreen.style.display = 'none';
+    homeScreen.style.display = 'flex';
+}
+
+function displayGameScreen() {
+    homeScreen.style.display = 'none';
+    bonusScreen.style.display = 'none';
+    gameScreen.style.display = 'block';
+};
+
+function displayBonusScreen() {
+    homeScreen.style.display = 'none';
+    gameScreen.style.display = 'none';
+    bonusScreen.style.display = 'block';
+}
+
+
 // Loop over Player buttons - Loop over each choice and return the ID of the button that was clicked, run game function.
 
 // Computer Choice Function - Computer is given an array and chooses a random choice.
