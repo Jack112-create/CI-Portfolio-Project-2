de6b619c2cb0032765eb8df519525693003f7085
Jack Conroy <jackconroy777@gmail.com>
2021-09-08 10:11:04 +0000

    Add pseudo code for game logic

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Loop over Player buttons - Loop over each choice and return the ID of the button that was clicked, run game function.
+
+// Computer Choice Function - Computer is given an array and chooses a random choice.
+
+// Game Function - Check the User choice vs computer, increment score.
+
+// Game Reset Function - Reset both User and Computer score.
+
+// Winner Modal Function - Modal appears when either player reaches 5 points.
+
+// Bonus Game Function - Check if the User choice of language corresponds to code displayed by Computer.
