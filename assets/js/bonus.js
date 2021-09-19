c4eb20a596e6c3259d0d60bc09352462857e36df
Jack Conroy <jackconroy777@gmail.com>
2021-09-19 11:13:21 +0000

    Add generate question function

--- assets/js/bonus.js
+++ assets/js/bonus.js
@@ -10,4 +10,28 @@ for (let language of languages) {
 function bonusGame(e) {
     userChoice = e.target.id;
     console.log(userChoice);
+    generateQuestion();
 };
+
+function generateQuestion() {
+    const code = document.getElementById('computer-code');
+    const questions = ['html', 'css', 'js', 'python'];
+    question = questions[Math.floor(Math.random() * questions.length)];
+    console.log('question:', question);
+
+    code.className = `language-${question}`;
+
+    if(question === 'html') {
+        code.innerHTML = '&lt;h1&gt;Hello World&lt;/h1&gt';
+    } else if(question === 'css') {
+        code.innerHTML = 'p {color: red}';
+    } else if(question === 'js') {
+        code.innerHTML = 'function() {}';
+    } else {
+        code.innerHTML = 'Print()';
+    }
+
+    return question;
+};
+
+generateQuestion();
