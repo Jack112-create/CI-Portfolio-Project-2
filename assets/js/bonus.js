4fa4655395cd44e9c87a849a6c583c03efd5bffe
Jack Conroy <jackconroy777@gmail.com>
2021-09-19 11:11:18 +0000

    Add loop for language options with bonus game function

--- assets/js/bonus.js
+++ assets/js/bonus.js
@@ -1,3 +1,13 @@
 // Global Variables
 const languages = document.getElementsByClassName('language');
+userChoice;
 let question;
+
+for (let language of languages) {
+   language.addEventListener('click', bonusGame);
+};
+
+function bonusGame(e) {
+    userChoice = e.target.id;
+    console.log(userChoice);
+};
